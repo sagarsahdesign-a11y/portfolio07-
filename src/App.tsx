f11227e72b1313127c670a5d74c5f4ec0e7c2936
Sagar Sah <sagar.sah.design@gmail.com>
2026-02-17 17:42:53 +0530

    fix: switch to HashRouter for GitHub Pages static hosting compatibility

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import Resume from "./pages/Resume";
 import Philosophy from "./pages/Philosophy";
@@ -19,7 +19,7 @@ const App = () => (
         <div className="relative z-10">
           <Toaster />
           <Sonner />
-          <BrowserRouter basename="/portfolio07-/">
+          <HashRouter>
             <Routes>
               <Route path="/" element={<Index />} />
               <Route path="/resume" element={<Resume />} />
@@ -28,7 +28,7 @@ const App = () => (
               <Route path="/services" element={<Services />} />
               <Route path="*" element={<NotFound />} />
             </Routes>
-          </BrowserRouter>
+          </HashRouter>
         </div>
       </div>
     </TooltipProvider>
